90100f3ce9eb2f80137089d52eef980ba15e24c4
Vismaya Ajikumar <92380865+vismayaajikumar@users.noreply.github.com>
2024-03-11 20:54:54 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES2UG21CS613 - changed\n";
+  cout << "PES2UG21CS613 - changed 2\n";
   return 0;
 }
